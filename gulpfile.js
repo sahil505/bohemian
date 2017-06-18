b7c85ecc6d0a252dea8b88db777442b07088114e
Sahil Khokhar <sahilkhokhar505@gmail.com>
2017-06-18 13:22:16 +0530

    installed dependencies & setup gulp workflow

---
+++ gulpfile.js
@@ -0,0 +1,17 @@
+'use strict';
+
+var del = require('del');
+var gulp = require('gulp');
+var autoprefixer = require('gulp-autoprefixer');
+var sass = require('gulp-sass');
+var rename = require('gulp-rename');
+var sourcemaps = require('gulp-sourcemaps');
+var cssnano = require('gulp-cssnano');
+var postcss = require('gulp-postcss');
+var sorting = require('postcss-sorting');
+
+// Config
+var build = {
+	css: './assets/css',
+	scss: './src/scss/'
+};
