9c7ef988a752a918d2e0dfeaa33f5c42c45ad728
Korku02 <korkudeepak@gmail.com>
2017-08-08 03:44:29 +0530

    events apis linked login and register working

--- dist/js/boxer.js
+++ dist/js/boxer.js
@@ -1,4 +1,304 @@
-app.controller('MainCtrl', function($scope, $document,$timeout, $log) {
+app.controller('CompetetionCtrl', function($scope,$http, $document,$timeout, $log, Auth,$location,$mdToast,$rootScope) {
+
+$scope.isDance = true;
+
+$scope.danceIndex = 0;
+$scope.dramaticsIndex = 0;
+$scope.musicIndex = 0;
+$scope.pfcIndex = 0;
+$scope.comedyIndex = 0;
+$scope.adventureIndex = 0;
+$scope.culinaryIndex = 0;
+$scope.debateIndex = 0;
+$scope.pfcIndex = 0;
+$scope.facIndex = 0;
+$scope.glamourIndex = 0;
+$scope.hindiIndex = 0;
+$scope.literaryIndex = 0;
+$scope.magicIndex = 0;
+$scope.quizIndex = 0;
+$scope.init = function(){
+
+  $http({
+      method: "GET",
+      url: URL_PREFIX+"category-event/dance",
+      headers: {
+        'Content-Type': 'application/json; charset=UTF-8',
+          }
+  }).then(function (response) {
+      $scope.isDance = false;
+      $scope.danceData = response.data.events;
+  });
+
+  $http({
+      method: "GET",
+      url: URL_PREFIX+"category-event/dramatics",
+      headers: {
+        'Content-Type': 'application/json; charset=UTF-8',
+          }
+  }).then(function (response) {
+      $scope.isDance = false;
+      $scope.dramaticsData = response.data.events;
+  });
+
+  $http({
+      method: "GET",
+      url: URL_PREFIX+"category-event/music",
+      headers: {
+        'Content-Type': 'application/json; charset=UTF-8',
+          }
+  }).then(function (response) {
+      $scope.isDance = false;
+      $scope.musicData = response.data.events;
+
+  });
+  $http({
+      method: "GET",
+      url: URL_PREFIX+"category-event/pfc",
+      headers: {
+        'Content-Type': 'application/json; charset=UTF-8',
+          }
+  }).then(function (response) {
+      $scope.isDance = false;
+      $scope.pfcData = response.data.events;
+
+  });
+  $http({
+      method: "GET",
+      url: URL_PREFIX+"category-event/comedy",
+      headers: {
+        'Content-Type': 'application/json; charset=UTF-8',
+          }
+  }).then(function (response) {
+      $scope.isDance = false;
+      $scope.comedyData = response.data.events;
+
+  });
+
+  $http({
+      method: "GET",
+      url: URL_PREFIX+"category-event/adventure",
+      headers: {
+        'Content-Type': 'application/json; charset=UTF-8',
+          }
+  }).then(function (response) {
+      $scope.isDance = false;
+      $scope.adventureData = response.data.events;
+
+  });
+
+  $http({
+      method: "GET",
+      url: URL_PREFIX+"category-event/culinary",
+      headers: {
+        'Content-Type': 'application/json; charset=UTF-8',
+          }
+  }).then(function (response) {
+      $scope.isDance = false;
+      $scope.culinaryData = response.data.events;
+
+  });
+
+  $http({
+      method: "GET",
+      url: URL_PREFIX+"category-event/debate",
+      headers: {
+        'Content-Type': 'application/json; charset=UTF-8',
+          }
+  }).then(function (response) {
+      $scope.isDance = false;
+      $scope.debateData = response.data.events;
+
+  });
+
+  $http({
+      method: "GET",
+      url: URL_PREFIX+"category-event/fac",
+      headers: {
+        'Content-Type': 'application/json; charset=UTF-8',
+          }
+  }).then(function (response) {
+      $scope.isDance = false;
+      $scope.facData = response.data.events;
+
+  });
+
+  $http({
+      method: "GET",
+      url: URL_PREFIX+"category-event/glamour",
+      headers: {
+        'Content-Type': 'application/json; charset=UTF-8',
+          }
+  }).then(function (response) {
+      $scope.isDance = false;
+      $scope.glamourData = response.data.events;
+
+  });
+
+  $http({
+      method: "GET",
+      url: URL_PREFIX+"category-event/hindi",
+      headers: {
+        'Content-Type': 'application/json; charset=UTF-8',
+          }
+  }).then(function (response) {
+      $scope.isDance = false;
+      $scope.hindiData = response.data.events;
+
+  });
+
+  $http({
+      method: "GET",
+      url: URL_PREFIX+"category-event/literary",
+      headers: {
+        'Content-Type': 'application/json; charset=UTF-8',
+          }
+  }).then(function (response) {
+      $scope.isDance = false;
+      $scope.literaryData = response.data.events;
+
+  });
+
+  $http({
+      method: "GET",
+      url: URL_PREFIX+"category-event/quiz",
+      headers: {
+        'Content-Type': 'application/json; charset=UTF-8',
+          }
+  }).then(function (response) {
+      $scope.isDance = false;
+      $scope.quizData = response.data.events;
+
+  });
+
+  $http({
+      method: "GET",
+      url: URL_PREFIX+"category-event/magic",
+      headers: {
+        'Content-Type': 'application/json; charset=UTF-8',
+          }
+  }).then(function (response) {
+      $scope.isDance = false;
+      $scope.magicData = response.data.events;
+
+  });
+
+
+}
+
+$scope.init();
+
+$scope.tabDance = function(a) {
+  // console.log(a);
+  $scope.danceIndex = a;
+
+};
+$scope.tabMusic = function(a) {
+  // console.log(a);
+  $scope.musicIndex = a;
+
+};
+$scope.tabPfc = function(a) {
+  // console.log(a);
+  $scope.pfcIndex = a;
+
+};
+$scope.tabDramatics = function(a) {
+  // console.log(a);
+  $scope.dramaticsIndex = a;
+
+}
+$scope.tabCulinary = function(a) {
+  // console.log(a);
+  $scope.culinaryIndex = a;
+
+}
+$scope.tabDebate = function(a) {
+  // console.log(a);
+  $scope.debateIndex = a;
+
+}
+$scope.tabFac = function(a) {
+  // console.log(a);
+  $scope.facIndex = a;
+
+}
+$scope.tabGlamour = function(a) {
+  // console.log(a);
+  $scope.glamourIndex = a;
+
+}
+$scope.tabHindi = function(a) {
+  // console.log(a);
+  $scope.hindiIndex = a;
+
+}
+$scope.tabLiterary = function(a) {
+  // console.log(a);
+  $scope.literaryIndex = a;
+
+}
+$scope.tabMagic = function(a) {
+  // console.log(a);
+  $scope.magicIndex = a;
+
+}
+$scope.tabQuiz = function(a) {
+  // console.log(a);
+  $scope.quizIndex = a;
+
+}
+$scope.tabAdventure = function(a) {
+  // console.log(a);
+  $scope.adventureIndex = a;
+
+}
+$scope.tabComedy = function(a) {
+  // console.log(a);
+  $scope.comedyIndex = a;
+
+}
+
+
+
+
+});
+
+app.controller('MainCtrl', function($scope, $document,$timeout, $log, Auth,$location,$mdToast,$rootScope) {
+
+  $rootScope.isPath= function(viewLocation) {
+      return viewLocation === $location.path();
+    };
+    $scope.userDetails = Auth.getuserFullDetails();
+
+    // console.log($scope.userDetails);
+
+  if($rootScope.isPath('/')===true && $scope.userDetails !=undefined){
+    $location.path("/home");
+
+  }
+
+
+    $scope.logInUser=function (user) {
+    if (user===undefined) {
+      $mdToast.show(
+        $mdToast.simple()
+        .textContent('Please check your input field')
+        .position('bottom right')
+        .hideDelay(3000)
+      );
+    }
+    Auth.login(user).then(function(response) {
+      $scope.userDetails = response;
+      $location.path("/home");
+      $mdToast.show(
+        $mdToast.simple()
+        .textContent('User sucessfully logged in!')
+        .position('bottom right')
+        .hideDelay(3000)
+      );
+    });
+  };
 
 $scope.bro = function(){
   console.log("working");
@@ -34,7 +334,7 @@ $scope.compevents=[
                 "col":1,
                 "class":"tile1",
                 "bg":"#FFECB3",
-                "href":"#!/competetions",
+                "href":"#!/dance",
                 "image":"url('images/pic01.jpg')"
               },
               {
@@ -43,7 +343,7 @@ $scope.compevents=[
                 "col":1,
                 "class":"tile1",
                 "bg":"#FF8A65",
-                "href":"#!/competetions",
+                "href":"#!/drama",
                 "image":"url('images/pic01.jpg')"
               },
               {
@@ -52,7 +352,7 @@ $scope.compevents=[
                 "col":1,
                 "class":"tile1",
                 "bg":"#E64A19",
-                "href":"#!/competetions",
+                "href":"#!/pfc",
                 "image":"url('images/pic01.jpg')"
               },
               {
@@ -61,7 +361,7 @@ $scope.compevents=[
                 "col":1,
                 "class":"tile1",
                 "bg":"#43A047",
-                "href":"#!/competetions",
+                "href":"#!/fac",
                 "image":"url('images/pic01.jpg')"
               },
               {
@@ -70,7 +370,7 @@ $scope.compevents=[
                 "col":1,
                 "class":"tile1",
                 "bg":"#512DA8",
-                "href":"#!/competetions",
+                "href":"#!/music",
                 "image":"url('images/pic01.jpg')"
               },
               {
@@ -79,7 +379,7 @@ $scope.compevents=[
                 "col":1,
                 "class":"tile1",
                 "bg":"#4A148C",
-                "href":"#!/competetions",
+                "href":"#!/debate",
                 "image":"url('images/pic01.jpg')"
               },
               {
@@ -88,7 +388,7 @@ $scope.compevents=[
                 "col":1,
                 "class":"tile1",
                 "bg":"#ef5350",
-                "href":"#!/competetions",
+                "href":"#!/literary",
                 "image":"url('images/pic01.jpg')"
               },
               {
@@ -97,7 +397,7 @@ $scope.compevents=[
                 "col":1,
                 "class":"tile1",
                 "bg":"#536DFE",
-                "href":"#!/competetions",
+                "href":"#!/quiz",
                 "image":"url('images/pic01.jpg')"
               },
               {
@@ -106,7 +406,7 @@ $scope.compevents=[
                 "col":1,
                 "class":"tile1",
                 "bg":"#00ACC1",
-                "href":"#!/competetions",
+                "href":"#!/hindi",
                 "image":"url('images/pic01.jpg')"
               },
               {
@@ -115,7 +415,7 @@ $scope.compevents=[
                 "col":1,
                 "class":"tile1",
                 "bg":"#00695C",
-                "href":"#!/competetions",
+                "href":"#!/magic",
                 "image":"url('images/pic01.jpg')"
               },
               {
@@ -124,7 +424,7 @@ $scope.compevents=[
                 "col":1,
                 "class":"tile1",
                 "bg":"#424242",
-                "href":"#!/competetions",
+                "href":"#!/comedy",
                 "image":"url('images/pic01.jpg')"
               },
               {
@@ -133,7 +433,7 @@ $scope.compevents=[
                 "col":1,
                 "class":"tile1",
                 "bg":"#FFECB3",
-                "href":"#!/competetions",
+                "href":"#!/glamour",
                 "image":"url('images/pic01.jpg')"
               },
               {
@@ -142,7 +442,7 @@ $scope.compevents=[
                 "col":1,
                 "class":"tile1",
                 "bg":"#ff1744",
-                "href":"#!/competetions",
+                "href":"#!/adventure",
                 "image":"url('images/pic01.jpg')"
               },
               {
@@ -151,7 +451,7 @@ $scope.compevents=[
                 "col":1,
                 "class":"tile1",
                 "bg":"#FF8F00",
-                "href":"#!/competetions",
+                "href":"#!/culinary",
                 "image":"url('images/pic01.jpg')"
               }
 ];
@@ -184,6 +484,155 @@ $scope.pronitesCarousel = [
 ];
 
 
+
+
+
+
+
+});
+
+app.controller('RegisterCtrl', function($scope, $document,$timeout, $log,$mdToast,$http,$mdDialog,$rootScope,$window,$location,Auth) {
+
+  $scope.userDetails = Auth.getuserFullDetails();
+
+  $rootScope.isPath= function(viewLocation) {
+      return viewLocation === $location.path();
+    };
+
+  $rootScope.showAdvanced = function(ev) {
+      $mdDialog.show({
+        controller: DialogController,
+        templateUrl: '../templates/otpdialog.html',
+        parent: angular.element(document.body),
+        targetEvent: ev,
+        clickOutsideToClose:true,
+      })
+      .then(function(answer) {
+        $scope.status = 'You said the information was "' + answer + '".';
+
+
+
+      }, function() {
+        $scope.status = 'You cancelled the dialog.';
+      });
+    };
+
+
+    function DialogController($scope, $mdDialog) {
+
+      $scope.hide = function() {
+        $mdDialog.hide();
+      };
+
+      $scope.cancel = function() {
+        $mdDialog.cancel();
+      };
+
+      $scope.answer = function(answer) {
+        $mdDialog.hide(answer);
+      };
+    };
+  $scope.signUp=function (user) {
+    $rootScope.preuser = user;
+      if(user.password == user.confirm_password){
+        console.log(user);
+        $rootScope.showAdvanced();
+
+      $http({
+        url:URL_PREFIX+"register/",
+        method:"POST",
+        headers:{
+          'Content-Type': 'application/json; charset=UTF-8'
+        },
+        data:{
+          'email':user.email,
+          'first_name':user.firstname,
+          'last_name':user.lastname,
+          'password':user.password
+        }
+      }).then(function sucessCallback(response) {
+        console.log(response);
+        if (response.status===200){
+          console.log(response);
+          // $location.path("/login");
+          $mdToast.show(
+            $mdToast.simple()
+            .textContent(response)
+            .position('bottom right')
+            .hideDelay(3000)
+          );
+        }
+      }, function errorCallback(error) {
+        console.log(error);
+        if (error.status===302){
+          $mdToast.show(
+            $mdToast.simple()
+            .textContent('Something went wrong, Please try again!')
+            .position('bottom right')
+            .hideDelay(3000)
+          );
+        }
+      });
+    }
+
+    else{
+      $mdToast.show(
+        $mdToast.simple()
+        .textContent('Please Confirm password')
+        .position('bottom right')
+        .hideDelay(3000)
+      );
+    }
+    };
+
+  $scope.submitOtp = function(user){
+    var preuser = $rootScope.preuser;
+    console.log(user);
+    console.log(preuser);
+    $http({
+      url:URL_PREFIX+"register/",
+      method:"POST",
+      headers:{
+        'Content-Type': 'application/json; charset=UTF-8'
+      },
+      data:{
+        'email':preuser.email,
+        'first_name':preuser.firstname,
+        'last_name':preuser.lastname,
+        'password':preuser.password,
+        'otp':user.otp
+      }
+    }).then(function sucessCallback(response) {
+      console.log(response);
+      userFullDetails = {
+          first_name: response.data.user.first_name,
+          last_name: response.data.user.last_name,
+          rdv_number: response.data.user.rdv_number,
+          access_token:response.data.token,
+      };
+      $window.localStorage.userFullDetails = JSON.stringify(userFullDetails);
+      if (response.status===200){
+        console.log(response);
+        $location.path("/home");
+        $mdToast.show(
+          $mdToast.simple()
+          .textContent("Successfully Logged In")
+          .position('bottom right')
+          .hideDelay(3000)
+        );
+      }
+    }, function errorCallback(error) {
+      if (error.status===401){
+        $mdToast.show(
+          $mdToast.simple()
+          .textContent('Wrong OTP')
+          .position('bottom right')
+          .hideDelay(3000)
+        );
+      }
+    });
+
+  };
 
 
 
--- dist/main.js
+++ dist/main.js
@@ -1,25 +1,154 @@
 var app = angular.module('boxer', ['ngMaterial','ngRoute','ngAnimate','duScroll','angular-scroll-animate','ngParallax','angular-carousel']);
 
-
+var URL_PREFIX = 'http://rdv-iitd.com/api/'
 // routes for the urls
 app.config(["$routeProvider", "$locationProvider", function($routeProvider, $locationProvider) {
   $routeProvider.when("/", {
     controller: "MainCtrl",
-    templateUrl: "templates/home.html"
+    templateUrl: "templates/login.html"
   }).when("/register", {
-    controller: "MainCtrl",
+    controller: "RegisterCtrl",
     templateUrl: "templates/register.html"
-  }).when("/competetions", {
+  }).when("/home", {
     controller: "MainCtrl",
-    templateUrl: "templates/competetion.html"
+    templateUrl: "templates/home.html"
   }).when("/compevents", {
     controller: "MainCtrl",
     templateUrl: "templates/compevents.html"
   }).when("/pronites", {
     controller: "MainCtrl",
     templateUrl: "templates/pronites.html"
+  }).when("/dance", {
+    controller: "CompetetionCtrl",
+    templateUrl: "templates/dance.html"
+  }).when("/music", {
+    controller: "CompetetionCtrl",
+    templateUrl: "templates/music.html"
+  }).when("/drama", {
+    controller: "CompetetionCtrl",
+    templateUrl: "templates/drama.html"
+  }).when("/pfc", {
+    controller: "CompetetionCtrl",
+    templateUrl: "templates/pfc.html"
+  }).when("/adventure", {
+    controller: "CompetetionCtrl",
+    templateUrl: "templates/adventure.html"
+  }).when("/quiz", {
+    controller: "CompetetionCtrl",
+    templateUrl: "templates/quiz.html"
+  }).when("/culinary", {
+    controller: "CompetetionCtrl",
+    templateUrl: "templates/culinary.html"
+  }).when("/magic", {
+    controller: "CompetetionCtrl",
+    templateUrl: "templates/magic.html"
+  }).when("/literary", {
+    controller: "CompetetionCtrl",
+    templateUrl: "templates/literary.html"
+  }).when("/glamour", {
+    controller: "CompetetionCtrl",
+    templateUrl: "templates/glamour.html"
+  }).when("/hindi", {
+    controller: "CompetetionCtrl",
+    templateUrl: "templates/hindi.html"
+  }).when("/debate", {
+    controller: "CompetetionCtrl",
+    templateUrl: "templates/debate.html"
+  }).when("/fac", {
+    controller: "CompetetionCtrl",
+    templateUrl: "templates/fac.html"
+  }).when("/comedy", {
+    controller: "CompetetionCtrl",
+    templateUrl: "templates/comedy.html"
   }).otherwise({
     controller: "MainCtrl",
     templateUrl: "templates/error.html"
   });
+}]);
+
+
+
+app.run(["$rootScope", "$location", function ($rootScope, $location) {
+    $rootScope.$on("$routeChangeSuccess", function (userFullDetails) {
+        console.log(userFullDetails);
+    });
+    $rootScope.$on("$routeChangeError", function (event, current, previous, eventObj) {
+        if (eventObj.authenticated === false) {
+            $location.path("/");
+        }
+    });
+}]);
+app.factory("Auth", ["$http","$q","$window",function ($http, $q, $window) {
+    var userFullDetails;
+    function login(user) {
+        var url=URL_PREFIX+'login/';
+        var deferred = $q.defer();
+        $http({
+             method: "POST",
+            //  transformRequest: function(obj) {
+            //     var str = [];
+            //     for(var p in obj)
+            //     str.push(encodeURIComponent(p) + "=" + encodeURIComponent(obj[p]));
+            //     return str.join("&");
+            //  },
+             data: {
+                'login_id':user.username,
+                'password':user.password
+
+             },
+             headers: {
+                'Content-Type': 'application/json; charset=UTF-8'
+              },
+             url: url,
+           }).then(function successCallback(response) {
+             isLoading = false;
+             console.log(response);
+             userFullDetails = {
+                 first_name: response.data.user.first_name,
+                 last_name: response.data.user.last_name,
+                 rdv_number: response.data.user.rdv_number,
+                 access_token:response.data.token,
+             };
+             $window.localStorage.userFullDetails = JSON.stringify(userFullDetails);
+             deferred.resolve(userFullDetails);
+           }, function errorCallback(error) {
+             deferred.reject(error);
+         });
+         return deferred.promise;
+    };
+
+    function logout() {
+        var deferred = $q.defer();
+        $http({
+            method: "POST",
+            url: URL_PREFIX+"logout/",
+            headers: {
+              'Content-Type': 'application/json; charset=UTF-8',
+              'Authorization':'Token '+userFullDetails.access_token
+            }
+        }).then(function (result) {
+            // console.log(result);
+            userFullDetails = null;
+            $window.localStorage.userFullDetails = null;
+            deferred.resolve(result);
+        }, function (error) {
+            deferred.reject(error);
+        });
+        return deferred.promise;
+    };
+
+    function getuserFullDetails() {
+        return userFullDetails;
+    }
+    function init() {
+        if ($window.localStorage.userFullDetails) {
+            userFullDetails = JSON.parse($window.localStorage.userFullDetails);
+        }
+    }
+    init();
+    return {
+        login: login,
+        logout: logout,
+        getuserFullDetails: getuserFullDetails
+    };
 }]);
